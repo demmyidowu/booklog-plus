be3fda3814fdf35a3c864fd758e98d4c2f94cc38
Demmy <didowu123@gmail.com>
2025-06-04 16:40:45 -0500

    fixed email redirect

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -43,7 +43,7 @@ export default function SignUpPage({ onNavigateToSignIn }) {
           data: {
             name: formData.name,
           },
-          emailRedirectTo: window.location.origin
+          emailRedirectTo: `${window.location.origin}/signin`
         },
       })
 
